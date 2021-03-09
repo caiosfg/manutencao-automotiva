a6bf7dbbdbcbf3f2aa41c99f7ac105767caadb59
caiosfg <caio.sfg@gmail.com>
2021-03-09 13:55:16 -0300

    inserido algumas validações, e o uso de tratativas na resposta

--- app/Http/Controllers/CarrosController.php
+++ app/Http/Controllers/CarrosController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Carros;
-
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class CarrosController extends Controller
@@ -20,21 +22,53 @@ class CarrosController extends Controller
     {
         $carros = $this->model->all();
 
-       return response()->json($carros);
+        if (count($carros) > 0) {
+            return response()->json($carros, Response::HTTP_OK);
+        } else {
+            return response()->json([], Response::HTTP_OK);
+        }
+       
     }
 
     public function get($id)
     {
        $carro = $this->model->find($id);
 
-       return response()->json($carro);
+       if (count($carro) > 0) {
+        return response()->json($carro, Response::HTTP_OK);
+       } else {
+        return response()->json(null, Response::HTTP_OK);
+       }
+       
     }
 
     public function store(Request $request)
     {
-        $carro = $this->model->create($request->all());
 
-        return response()->json($carro);
+        //Validator recebe o primeiro parametro de post, depois o array de validação de dados
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required | max: 80',
+                'description' => 'required',
+                'model' => 'required | max: 10 | min: 2',
+                'date' => 'required | date_format : "Y-m-d'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response:: HTTP_BAD_REQUEST);
+        } else {
+            
+            try {
+                $carro = $this->model->create($request->all());
+                return response()->json($carro, Response:: HTTP_CREATED);
+            } catch (QueryException $exception) {
+                return response()->json(['error' => 'Erro de comunicação com a base',
+                Response::HTTP_BAD_GATEWAY]);
+            }
+        }
+
     }
 
     public function update($id, Request $request)
@@ -43,6 +77,8 @@ class CarrosController extends Controller
                ->update($request->all());
 
         return response()->json($carro);
+
+        
     }
 
     public function destroy($id)
@@ -50,6 +86,6 @@ class CarrosController extends Controller
         $carro = $this->model->find($id)
                ->delete();
         
-        return response()->json(data: null);
+        return response()->json(null);
     }
 }
