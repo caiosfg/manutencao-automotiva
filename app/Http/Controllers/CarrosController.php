7d5a9db9910eb5b9e8a0758f089ea5faed91a872
caiosfg <caio.sfg@gmail.com>
2021-03-13 12:52:09 -0300

    inicio das melhorias

--- app/Http/Controllers/CarrosController.php
+++ app/Http/Controllers/CarrosController.php
@@ -6,6 +6,7 @@ use App\Models\Carros;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\ValidationCar;
 
 
 class CarrosController extends Controller
@@ -73,12 +74,24 @@ class CarrosController extends Controller
 
     public function update($id, Request $request)
     {
-        $carro = $this->model->find($id)
-               ->update($request->all());
+        $validator =  ValidationCar::make(
+            $request->all(),
+            ValidationCar::RULE_CAR
+        );
 
-        return response()->json($carro);
+        if ($validator->fails()) {
+            return response()->json($validator->error(), Response::HTTP_BAD_REQUEST);
+        } else {
+            try {
+                $carro = $this->model->find($id)
+                ->update($request->all());
 
-        
+                return response()->json($carro, Response::HTTP_OK);
+            } catch (QueryException $exception) {
+                return response()->json(['error' => 'Erro de conexão com Banco'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+   
     }
 
     public function destroy($id)
